c9f1e072c3bfead958c9b0f1a9097e1ac5d39eab
natlllu <jingjiahui.lu@student.unimelb.edu.au>
2021-10-23 23:18:40 +0800

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,132 @@
 import React, { FC, ChangeEvent, useState } from "react";
-import "./App.css";
 import TodoTask from "./Components/TodoTask";
 import { ITask } from "./Interface";
+import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import MuiDrawer from "@mui/material/Drawer";
+import {
+  styled,
+  useTheme,
+  Theme,
+  CSSObject,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import Divider from "@mui/material/Divider";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import { DateTimePicker } from "@mui/lab";
+import List from "@mui/material/List";
 
+import {
+  Button,
+  Container,
+  CssBaseline,
+  InputLabel,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+
+const drawerWidth = 240;
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(9)} + 1px)`,
+  },
+});
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+export const light = {
+  palette: {
+    type: "light",
+  },
+};
+export const dark = {
+  palette: {
+    type: "dark",
+  },
+};
 const App: FC = () => {
+  const theme = useTheme();
+  const [darkState, setDarkState] = useState(false);
+
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
@@ -14,6 +137,21 @@ const App: FC = () => {
     } else {
       setDeadline(Number(event.target.value));
     }
+  };
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const [value, setValue] = React.useState<Date | null>(new Date());
+
+  const handleChange1 = (newValue: Date | null) => {
+    setValue(newValue);
   };
 
   const addTask = (): void => {
@@ -27,31 +165,87 @@ const App: FC = () => {
   const handDelete = (taskNameToDelte: string): void => {
     setTodoList(
       todoList.filter((t) => {
-        return t.taskName != taskNameToDelte;
+        return t.taskName !== taskNameToDelte;
       })
     );
   };
-
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
   return (
-    <div className="App">
-      <div className="header">
-        <div className="inputContainer">
-          <input
-            type="text"
-            placeholder="Task..."
-            onChange={handleChange}
-            name="task"
-            value={task}
-          ></input>
-          <input
-            type="number"
-            placeholder="Deasline (in Days)..."
-            name="deadline"
-            value={deadline}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <button onClick={addTask}>add task</button>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: "36px",
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography>Task Schedular</Typography>
+          <Switch defaultChecked color="secondary" />{" "}
+        </Toolbar>
+      </AppBar>
+
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <div>
+        <Container maxWidth="lg">
+          <FormControl>
+            <InputLabel>Task</InputLabel>
+            <Input id="my-input" aria-describedby="my-helper-text" />
+            <br></br>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DateTimePicker
+                label="DeadLine"
+                value={value}
+                onChange={handleChange1}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </FormControl>
+        </Container>
       </div>
 
       <div className="todoList">
@@ -59,7 +253,7 @@ const App: FC = () => {
           return <TodoTask key={key} task={task} handDelete={handDelete} />;
         })}
       </div>
-    </div>
+    </Box>
   );
 };
 
